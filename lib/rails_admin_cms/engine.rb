5520524178a7c7333312f241a81d80385456a492
Patrice Lebel <patrice.lebel@outlook.com>
2015-12-26 21:03:40 -0500

    initial migrations added by default to app migration paths

--- lib/rails_admin_cms/engine.rb
+++ lib/rails_admin_cms/engine.rb
@@ -1,4 +1,11 @@
 module RailsAdminCMS
   class Engine < ::Rails::Engine
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end

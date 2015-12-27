473ee05db27837fd951d2579d43fb7de1c4b17a3
Patrice Lebel <patrice.lebel@outlook.com>
2015-12-26 20:59:55 -0500

    added gems required inside the engine

--- lib/rails_admin_cms.rb
+++ lib/rails_admin_cms.rb
@@ -1,3 +1,8 @@
+require "rails_admin"
+require "route_translator"
+require "active_link_to"
+require "youtube_addy"
+
 require "rails_admin_cms/engine"
 require "rails_admin_cms/inflections"
 require "rails_admin_cms/core_ext/boolean"

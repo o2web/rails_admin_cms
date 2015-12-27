55a7fa42fa0ae36a8fa69e7717d3ece1251905c0
Patrice Lebel <patrice.lebel@outlook.com>
2015-12-26 22:18:12 -0500

    corrected view_helper_spec for edit_mode?

--- spec/helpers/view_helper_spec.rb
+++ spec/helpers/view_helper_spec.rb
@@ -5,15 +5,16 @@ RSpec.describe CMS::ViewHelper, type: :helper do
     before(:each) do
       I18n.locale = :fr
       allow(controller).to receive(:action_name).and_return('show')
-      allow(params).to receive(:[]).with(:cms_body_class).and_return('show')
+      allow(params).to receive(:[]).with(:cms_body_class).and_return('page')
+      helper.class.send(:define_method, :edit_mode?) { true }
     end
 
     it "generate contextual classes" do
-      expect(helper.cms_body_class).to eql("show cms-view cms-view-show fr")
+      expect(helper.cms_body_class).to eql("page cms-view cms-view-show fr cms-edit-mode")
     end
 
     it "adds other classes" do
-      expect(helper.cms_body_class('one', 'two')).to eql("show cms-view cms-view-show fr one two")
+      expect(helper.cms_body_class('one', 'two')).to eql("page cms-view cms-view-show fr cms-edit-mode one two")
     end
   end
 end

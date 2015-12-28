2e7e6f374ad502dc16633f8893d9a810ab178e94
Patrice Lebel <patrice.lebel@outlook.com>
2015-12-28 17:54:48 -0500

    specs added for ViewablesController#update

--- spec/controllers/viewables_controller_spec.rb
+++ spec/controllers/viewables_controller_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 describe CMS::ViewablesController, type: :controller do
-  describe "GET create" do
-    before(:each) do
-      request.env["HTTP_REFERER"] = '/page'
-      @params = { list_key: {viewable_type: 'Viewable::Link', view_path: 'cms/pages/page', name: 'test', locale: 'fr' } }
-    end
+  before(:each) do
+    request.env["HTTP_REFERER"] = '/page'
+  end
+
+  describe "#create" do
+    let(:params) { { list_key: { viewable_type: 'Viewable::Link', view_path: 'cms/pages/page', name: 'test', locale: 'fr' } } }
 
     context "when count = 0 and max = 1" do
       it "creates 1 viewable to edit" do
-        get :create, @params.merge(max: 1)
+        get :create, params.merge(max: 1)
         expect(response).to redirect_to "http://test.host/admin/viewable~link/1/edit"
       end
     end
@@ -17,7 +18,7 @@ describe CMS::ViewablesController, type: :controller do
     context "when count = 1 and max = 1" do
       it "creates no new viewable" do
         FactoryGirl.create(:unique_key)
-        get :create, @params.merge(max: 1)
+        get :create, params.merge(max: 1)
         expect(response).to redirect_to "/page"
       end
     end
@@ -25,7 +26,7 @@ describe CMS::ViewablesController, type: :controller do
     context "when count = 2 and max = 1" do
       it "creates no new viewable" do
         FactoryGirl.create_list(:unique_key, 2)
-        get :create, @params.merge(max: 1)
+        get :create, params.merge(max: 1)
         expect(response).to redirect_to "/page"
       end
     end
@@ -33,8 +34,34 @@ describe CMS::ViewablesController, type: :controller do
     context "when count = 2 and max = Infinity" do
       it "creates 1 viewable to edit " do
         FactoryGirl.create_list(:unique_key, 2)
-        get :create, @params.merge(max: 'Infinity')
+        get :create, params.merge(max: 'Infinity')
         expect(response).to redirect_to "http://test.host/admin/viewable~link/3/edit"
+      end
+    end
+  end
+
+  describe "#update" do
+    context "when count = 3" do
+      before(:each) do
+        FactoryGirl.reload
+        FactoryGirl.create_list(:unique_key, 3)
+      end
+
+      subject { UniqueKey.all.map(&:position) }
+
+      it "updates position from 1 to 3" do
+        post :update, { id: 1, unique_key: { position: 3 } }
+        expect(subject).to eql([3,1,2])
+      end
+
+      it "updates position from 3 to 1" do
+        post :update, { id: 3, unique_key: { position: 1 } }
+        expect(subject).to eql([2,3,1])
+      end
+
+      it "updates position from 2 to 2" do
+        post :update, { id: 2, unique_key: { position: 2 } }
+        expect(subject).to eql([1,2,3])
       end
     end
   end

e65cb3845cc366e9c6341a831514b8844eb14eaf
Patrice Lebel <patrice.lebel@outlook.com>
2015-12-30 16:40:52 -0500

    css classes added for edit link action and name

--- app/presenters/viewable_presenter.rb
+++ app/presenters/viewable_presenter.rb
@@ -3,7 +3,11 @@ class ViewablePresenter < BasePresenter
     return unless h.edit_mode?
 
     h.link_to edit_path, class: "cms-edit cms-edit-#{m.dashed_name}", 'data-no-turbolink' => true do
-      "#{h.t('cms.edit')} #{name}"
+      h.concat h.content_tag(:span, h.t('cms.edit'), class: "cms-edit-action")
+      if name.present?
+        h.concat " "
+        h.concat h.content_tag(:span, name, class: "cms-edit-name")
+      end
     end
   end
 

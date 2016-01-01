975973c9dedb3c1b1a9ea536083484eff2c0ce79
Patrice Lebel <patrice.lebel@outlook.com>
2016-01-01 04:30:24 -0500

    block names are now fully restricted

--- app/models/viewable/block.rb
+++ app/models/viewable/block.rb
@@ -9,11 +9,21 @@ module Viewable
     end
 
     class << self
+      def restricted_names
+        @_restricted_names ||= Viewable.names + begin
+          CMS.constants.grep(/Helper$/).map{ |name|
+            "CMS::#{name}".constantize.instance_methods.grep(/^cms_/)
+          }.flatten.map{ |name|
+            name.to_s.sub(/^cms_/, '')
+          }
+        end
+      end
+
       def names
         @_names ||= begin
           Dir["#{Rails.root}/app/views/cms/blocks/*.html.*"].map do |name|
             name = File.basename(name).sub(/\.html\..+$/, '').sub(/^_/, '')
-            if name.in?(Viewable.names + %w[ cache ])
+            if name.in? restricted_names
               raise ArgumentError, "'cms/blocks/_#{name}.html' partial should be called otherwise, '#{name}' taken"
             end
             name

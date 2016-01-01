cd7b0c7e23ea63b3923da2f7753c0c230e6d66b3
Patrice Lebel <patrice.lebel@outlook.com>
2016-01-01 04:30:42 -0500

    title added to viewable_block (schema updated)

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20151231102148) do
 
   create_table "viewable_blocks", force: :cascade do |t|
     t.string   "uuid"
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

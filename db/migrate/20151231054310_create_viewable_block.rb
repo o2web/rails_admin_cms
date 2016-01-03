8a94d568d10d2cf125603022dcbac73a0143cecf
Patrice Lebel <patrice.lebel@outlook.com>
2016-01-02 21:47:31 -0500

    removed :title column in viewable_block

--- db/migrate/20151231054310_create_viewable_block.rb
+++ db/migrate/20151231054310_create_viewable_block.rb
@@ -2,7 +2,6 @@ class CreateViewableBlock < ActiveRecord::Migration
   def change
     create_table :viewable_blocks do |t|
       t.string     :uuid
-      t.string     :title
 
       t.timestamps null: false
     end

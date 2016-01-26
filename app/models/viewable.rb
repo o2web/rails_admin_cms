52c564c0c8ba481a8a3cd8b2936e4c7aa351121f
Patrice Lebel <patrice.lebel@outlook.com>
2016-01-26 16:03:58 -0500

    merged other_locale scope with localized scope

--- app/models/viewable.rb
+++ app/models/viewable.rb
@@ -18,8 +18,7 @@ module Viewable
     after_update :expire_cache
     after_touch  :expire_cache
 
-    scope :localized, -> { includes(:unique_key).where(unique_keys: { locale: I18n.locale }) }
-    scope :other_locale, ->(locale) { includes(:unique_key).where(unique_keys: { locale: locale }) }
+    scope :localized, ->(locale = I18n.locale) { includes(:unique_key).where(unique_keys: { locale: locale }) }
 
     delegate :has_unlocalized_fields?, :unlocalized_fields, :viewable_type, to: :class
   end

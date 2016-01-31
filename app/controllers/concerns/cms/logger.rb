71d4e193bdcede9a6a614bb57a03f16b9f236d04
Patrice Lebel <patrice.lebel@outlook.com>
2016-01-31 01:17:52 -0500

    cleaner exception logger

--- app/controllers/concerns/cms/logger.rb
+++ app/controllers/concerns/cms/logger.rb
@@ -6,14 +6,16 @@ module CMS
 
     def cms_logger(exception, log_name = nil)
       current_logger = log_name ? ::Logger.new("#{Rails.root}/log/#{log_name}.log") : logger
-      current_logger.error "\n[EXCEPTION][#{request.remote_ip}][#{request.method}][#{request.original_url}]"
+      current_logger.error ''
+      current_logger.error "[EXCEPTION][#{request.remote_ip}][#{request.method}][#{request.original_url}]"
       if exception
         current_logger.error exception.message
         exception.backtrace.first(RailsAdminCMS::Config.exception_backtrace_size).each do |line|
           current_logger.error line
         end
       end
-      current_logger.error "[END]\n"
+      current_logger.error '[END]'
+      current_logger.error ''
     end
   end
 end

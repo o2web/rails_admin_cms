604a06b19368205d8f173fb491da0bd5628353f4
Patrice Lebel <patrice.lebel@outlook.com>
2016-02-04 11:17:54 -0500

    clean-up pull request https://github.com/o2web/rails_admin_cms/commit/0010ec96a42efb310f45cba98009ecb084a9cc31

--- app/presenters/viewable/string_presenter.rb
+++
@@ -1,7 +0,0 @@
-module Viewable
-  class StringPresenter < ViewablePresenter
-    def text
-      h.strip_tags m.title
-    end
-  end
-end
--- db/migrate/20160201181310_create_viewable_string.rb
+++ db/migrate/20160201181310_create_viewable_string.rb
@@ -1,9 +1,9 @@
 class CreateViewableString < ActiveRecord::Migration
   def change
     create_table :viewable_strings do |t|
-        t.string     :title
+      t.string     :string
 
-        t.timestamps null: false
+      t.timestamps null: false
     end
   end
 end

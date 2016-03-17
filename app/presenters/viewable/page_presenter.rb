59974ad400a6ae0463d976bdef3ae78c1379c797
Guillaume Ferland <ferland182@gmail.com>
2016-03-17 11:30:10 -0400

    added custom controller show page title to breadcrumbs

--- app/presenters/viewable/page_presenter.rb
+++ app/presenters/viewable/page_presenter.rb
@@ -2,42 +2,44 @@ module Viewable
   class PagePresenter < ViewPresenter
 
     # build page tree from specified root_depth, default second level root
-    def tree(root_depth = 1, css_class = 'tree')
+    def tree(root_depth: 1, nav_class: 'tree')
       return if m.parent_at_depth(root_depth).nil?
-      h.content_tag :nav, class: css_class do
+      h.content_tag :nav, class: nav_class do
         h.concat render_tree_master_ul(m.parent_at_depth(root_depth))
       end
     end
 
     # build page breadcrumbs from specified root_depth, default root
-    def breadcrumbs(root_depth = 0, nav_class = 'breadcrumbs', div_class = 'scrollable')
+    def breadcrumbs(root_depth: 0, last_page_title: nil, nav_class: 'breadcrumbs', div_class: 'scrollable')
       return if m.parent_at_depth(root_depth).nil?
       h.content_tag :nav, class: nav_class do
-        h.concat h.content_tag(:div, breadcrumbs_ul(breadcrumbs_list(root_depth)), class: div_class)
+        h.concat h.content_tag(:div, breadcrumbs_ul(breadcrumbs_list(root_depth, last_page_title)), class: div_class)
       end
     end
 
     private
 
-    def breadcrumbs_list(root_depth = 0, page = m)
+    def breadcrumbs_list(root_depth = 0, last_page_title = nil, page = m)
+      pages = []
       while page.present? && page.depth >= root_depth
-        (pages ||= []).unshift(page)
+        pages.unshift(page)
         page = page.parent
       end
+      pages.push(OpenStruct.new({title: last_page_title})) if last_page_title.present?
       pages
     end
 
     def breadcrumbs_ul(pages)
       h.content_tag :ul do
         pages.each do |page|
-          h.concat(breadcrumbs_li(page))
+          h.concat(breadcrumbs_li(page, (pages.last == page)))
         end
       end
     end
 
-    def breadcrumbs_li(page)
+    def breadcrumbs_li(page, last_page)
       h.content_tag :li do
-        h.concat (!page.show_link) ? h.content_tag(:span, page.title) : h.link_to(page.title, page.url)
+        h.concat (page.show_link && !last_page) ? h.link_to(page.title, page.url) : h.content_tag(:span, page.title)
       end
     end
 

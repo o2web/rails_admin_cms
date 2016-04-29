df49f7826ebb393e4c45dd52b3ac72f422d629c9
Guillaume Ferland <ferland182@gmail.com>
2016-04-29 15:30:25 -0400

    added add show page meta method

--- app/controllers/cms/pages_controller.rb
+++ app/controllers/cms/pages_controller.rb
@@ -7,8 +7,9 @@ module CMS
       @cms_view = Viewable::Page.find(params[:page_id]) if params[:page_id].present? && params[:parent_id].blank?
     end
 
-    def find_page_parent_view
+    def find_page_parent_view(show_page = nil)
       @cms_view = Viewable::Page.find(params[:parent_id]) if params[:parent_id].present?
+      add_show_page_meta show_page unless show_page.nil?
     end
 
     def show
@@ -24,6 +25,24 @@ module CMS
     def allow_iframe
       if RailsAdminCMS::Config.allow_iframe_from.present?
         response.headers['X-FRAME-OPTIONS'] = RailsAdminCMS::Config.allow_iframe_from
+      end
+    end
+
+    def add_show_page_meta(show_page)
+      %i(
+        meta_title
+        meta_description
+        meta_keywords
+        meta_general_image
+        twitter_card
+        twitter_title
+        twitter_description
+        twitter_image
+        og_title
+        og_image
+        og_description
+      ).each do |meta|
+        @cms_view.send("#{meta}=", show_page.send(meta)) if show_page.send(meta).present?
       end
     end
   end

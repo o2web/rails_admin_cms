856756c033b10c810e15a03efb17fbc4a1c77f27
gloubier <gloubier@o2web.ca>
2016-06-01 08:35:11 -0400

    Don't create links on pages without "show_link"

--- app/presenters/viewable/page_presenter.rb
+++ app/presenters/viewable/page_presenter.rb
@@ -61,7 +61,7 @@ module Viewable
 
     def render_tree_li(page)
       h.content_tag(:li, class: ('active' if (page.subtree.include?(m) && !@sitemap))) do
-        h.concat h.link_to(page.title, page.url, class: ('active' if (page == m && !@sitemap)))
+        h.concat h.link_to_if(page.show_link?, page.title, page.url, class: ('active' if (page == m && !@sitemap)))
         page.children.order(:tree_position).each do |child|
           h.concat render_tree_ul(child)
         end if page.has_children? && (m.ancestors.include?(page) || m.subtree.include?(page) || @sitemap)
